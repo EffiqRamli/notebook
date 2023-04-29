6291db65cc423937a795ddf85b3ef44706f58090
EffiqRamli <83837445+EffiqRamli@users.noreply.github.com>
2023-04-30 03:01:48 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientist use are:
+#     1. Pyhton
+#     2. R
+#     3. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     1. TensorFlow
+#     2. NumPy
+#     3. Keras
+
+# | Data Science Tools |
+# | ------------------ |
+# | Jupyter Notebook |
+# | R |
+# | GitHub |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+
+200/60
+
+
+# **Objectives:**
+#     . Coding practices using Python
+#     . To undestand better on the interface of the notebook
+#     . Creating independent files using knowledge gain from the course
+
+# ## Author 
+# 
+# Effiq Ramli
+
+# In[ ]:
+
+
+
+
